c6d15569045c84e805bc49ef80ae46677f741b44
superQ78 <167489927+superQ78@users.noreply.github.com>
2025-12-11 01:45:13 -0700

    ComienzoSistema

---
+++ SistemaResidenciaItson/src/main/java/com/mycompany/sistemaresidenciaitson/SistemaResidenciaItson.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaresidenciaitson;
+
+/**
+ *
+ * @author cesar
+ */
+public class SistemaResidenciaItson {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
