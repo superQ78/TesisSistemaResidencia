53359dc98dad3af99f1a8deba163b5d79c34d2c9
ValeriaEL <valeria.encinas225330@potros.itson.edu.mx>
2026-01-30 16:00:26 -0700

    Paneles de datos

--- SistemaResidenciaItson/src/main/java/Presentacion/frmCrearResidente.java
+++ SistemaResidenciaItson/src/main/java/Presentacion/frmCrearResidente.java
@@ -262,6 +262,13 @@ public class frmCrearResidente extends javax.swing.JFrame {
         btnGuardarCrearResidente1 = new javax.swing.JButton();
         btnVaciarCamposCrearResi = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
         btnAtrasFrmRegistrarResi = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -1349,20 +1356,22 @@ public class frmCrearResidente extends javax.swing.JFrame {
                         .addGroup(javax.swing.GroupLayout.Alignment.LEADING, JpnlPrincipalLayout.createSequentialGroup()
                             .addContainerGap()
                             .addGroup(JpnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(JpnlDatosSolicitante, javax.swing.GroupLayout.DEFAULT_SIZE, 1256, Short.MAX_VALUE)
                                 .addComponent(JpnlDatosTutor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(JpnlDatosEmergencia, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addComponent(JpnlAspectosAcademicos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(JpnlPrincipalLayout.createSequentialGroup()
                         .addGap(64, 64, 64)
-                        .addComponent(JpnlDatosMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 1260, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(JpnlDatosMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 1260, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(JpnlPrincipalLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(JpnlDatosSolicitante, javax.swing.GroupLayout.PREFERRED_SIZE, 1256, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(368, Short.MAX_VALUE))
         );
         JpnlPrincipalLayout.setVerticalGroup(
             JpnlPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(JpnlPrincipalLayout.createSequentialGroup()
                 .addComponent(JpnlDatosSolicitante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(JpnlDatosTutor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(JpnlDatosEmergencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -1377,15 +1386,15 @@ public class frmCrearResidente extends javax.swing.JFrame {
 
         JcrlpCrearResi.setViewportView(JpnlPrincipal);
 
-        Jpnl.add(JcrlpCrearResi, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 1288, 480));
+        Jpnl.add(JcrlpCrearResi, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 130, 1270, 480));
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 30)); // NOI18N
         jLabel2.setText("Residencias ITSON – Panel de Gestión de residentes");
-        Jpnl.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(261, 29, -1, -1));
+        Jpnl.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 30, -1, -1));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 30)); // NOI18N
         jLabel1.setText("Registrar residente");
-        Jpnl.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(497, 76, -1, -1));
+        Jpnl.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 80, -1, -1));
 
         btnImprimirCrearresidente.setBackground(new java.awt.Color(51, 204, 255));
         btnImprimirCrearresidente.setFont(new java.awt.Font("Segoe UI", 1, 30)); // NOI18N
@@ -1396,39 +1405,146 @@ public class frmCrearResidente extends javax.swing.JFrame {
                 btnImprimirCrearresidenteActionPerformed(evt);
             }
         });
-        Jpnl.add(btnImprimirCrearresidente, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 690, 230, 40));
+        Jpnl.add(btnImprimirCrearresidente, new org.netbeans.lib.awtextra.AbsoluteConstraints(1120, 680, 230, 40));
 
         btnGuardarCrearResidente1.setBackground(new java.awt.Color(51, 204, 0));
         btnGuardarCrearResidente1.setFont(new java.awt.Font("Segoe UI", 1, 30)); // NOI18N
         btnGuardarCrearResidente1.setText("Guardar");
-        Jpnl.add(btnGuardarCrearResidente1, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 680, 230, 50));
+        Jpnl.add(btnGuardarCrearResidente1, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 670, 230, 50));
 
         btnVaciarCamposCrearResi.setBackground(new java.awt.Color(255, 51, 0));
         btnVaciarCamposCrearResi.setFont(new java.awt.Font("Segoe UI", 1, 30)); // NOI18N
         btnVaciarCamposCrearResi.setForeground(new java.awt.Color(255, 255, 255));
         btnVaciarCamposCrearResi.setText("Vaciar campos");
-        Jpnl.add(btnVaciarCamposCrearResi, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 690, 240, 40));
+        Jpnl.add(btnVaciarCamposCrearResi, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 680, 240, 40));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 22)); // NOI18N
         jLabel3.setText("Favor de imprimir, firmar, escanear y enviar por correo electrónico a: recepcionresidencias@itson.edu.mx");
-        Jpnl.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 760, -1, -1));
+        Jpnl.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 750, -1, -1));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 204, 255), new java.awt.Color(153, 204, 255), new java.awt.Color(153, 204, 255), new java.awt.Color(153, 204, 255)));
+
+        jButton1.setBackground(new java.awt.Color(153, 204, 255));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton1.setText("Datos del Tutor");
+        jButton1.setBorder(null);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setBackground(new java.awt.Color(153, 204, 255));
+        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton2.setText("Datos del Solicitante");
+        jButton2.setBorder(null);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setBackground(new java.awt.Color(153, 204, 255));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton3.setText("Contacto de Emergencia");
+        jButton3.setToolTipText("");
+        jButton3.setBorder(null);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setBackground(new java.awt.Color(153, 204, 255));
+        jButton4.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton4.setText("Aspectos Personales");
+        jButton4.setToolTipText("");
+        jButton4.setBorder(null);
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setBackground(new java.awt.Color(153, 204, 255));
+        jButton5.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton5.setText("Aspectos Academicos");
+        jButton5.setToolTipText("");
+        jButton5.setBorder(null);
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        jButton6.setBackground(new java.awt.Color(153, 204, 255));
+        jButton6.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton6.setText("Datos Medicos");
+        jButton6.setToolTipText("");
+        jButton6.setBorder(null);
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
 
         btnAtrasFrmRegistrarResi.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/atras.png"))); // NOI18N
-        Jpnl.add(btnAtrasFrmRegistrarResi, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+        btnAtrasFrmRegistrarResi.setBorder(null);
+        btnAtrasFrmRegistrarResi.setContentAreaFilled(false);
+        btnAtrasFrmRegistrarResi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAtrasFrmRegistrarResiActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAtrasFrmRegistrarResi)
+                    .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(26, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(btnAtrasFrmRegistrarResi)
+                .addGap(66, 66, 66)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(142, Short.MAX_VALUE))
+        );
+
+        Jpnl.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 270, 610));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(Jpnl, javax.swing.GroupLayout.PREFERRED_SIZE, 1475, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(Jpnl, javax.swing.GroupLayout.DEFAULT_SIZE, 1698, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(Jpnl, javax.swing.GroupLayout.PREFERRED_SIZE, 805, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(Jpnl, javax.swing.GroupLayout.PREFERRED_SIZE, 820, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -1597,6 +1713,34 @@ public class frmCrearResidente extends javax.swing.JFrame {
     }
     }//GEN-LAST:event_btnImprimirCrearresidenteActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void btnAtrasFrmRegistrarResiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAtrasFrmRegistrarResiActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAtrasFrmRegistrarResiActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -1724,10 +1868,17 @@ public class frmCrearResidente extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> cmbxExtensionCelTutor;
     private javax.swing.JComboBox<String> cmbxParentescoEmerg;
     private javax.swing.JComboBox<String> cmbxSemestreSoli;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel93;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblAcademicos1;
     private javax.swing.JLabel lblAcademicos1_1;
